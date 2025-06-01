f535ea36d4d32980d478ad39dcafa52f9732469f
FelipeBifaroni <felipe.bifaroni@gmail.com>
2025-06-01 10:25:51 -0300

    Update integral.js
    Correção, no calculo de integral.

--- funcoes/integral.js
+++ funcoes/integral.js
@@ -15,11 +15,10 @@ function regradoTrapezio(funcao, inicial, final, numeroDivisoes) {
 
     // Calcula o ponto entre todos os trapézios
     for (let i = 1; i < numeroDivisoes; i++) {
-        let x = Number(inicial + (i * h))   // Calcula os pontos de cada trapézio
+        let x = (Number(inicial) + (i * h))   // Calcula os pontos de cada trapézio
         soma += 2 * avaliarExpressao(funcao, x) // multiplica o calculo dos trapézios por 2 e acumulam eles na variavel soma
     }
     resultado = soma * (h / 2)
-
     return resultado.toFixed(4)
 }
 
@@ -34,7 +33,7 @@ function regradoSimpson(funcao, inicial, final, numeroDivisoes) {
 
     // Calcula o ponto entre todas as parábolas
     for (let i = 1; i < numeroDivisoes; i++) {
-        let x = Number(inicial + (i * h))   // Calcula os pontos de cada parábola
+        let x = (Number(inicial) + (i * h))   // Calcula os pontos de cada parábola
         let peso = (i % 2 === 0) ? 2 : 4    // Calcula o peso do número, 2 para impar e 4 para par
         soma += peso * avaliarExpressao(funcao, x) // multiplica o calculo das parábolas pelo peso e acumulam eles na variavel soma
     }
@@ -53,11 +52,11 @@ function regradoRetangulo(funcao, inicial, final, numeroDivisoes, tipo = 'meio')
     for (let i = 0; i < numeroDivisoes; i++) {
         let x 
         if (tipo === 'esquerda') {
-            x = Number(inicial + i * h)
+            x = (Number(inicial) + (i * h))
         } else if (tipo === 'direita') {
-            x = Number(inicial + (i + 1) * h)
+            x = (Number(inicial) + (i + 1) * h)
         } else { // meio
-            x = Number(inicial + (i + 0.5) * h)
+            x = (Number(inicial) + (i + 0.5) * h)
         }
         soma += avaliarExpressao(funcao, x) // Acumulam os cálculos na variavel soma
     }
